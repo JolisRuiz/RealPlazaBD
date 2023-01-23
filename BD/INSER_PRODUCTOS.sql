46afd150a01e271541b9b264831bc46826e41c9b
Jolis Ruiz <47835581+JolisRuiz@users.noreply.github.com>
2023-01-23 14:59:28 -0500

    primer commit

---
+++ BD/INSER_PRODUCTOS.sql
@@ -0,0 +1,18 @@
+USE [bd_plazavea]
+GO
+
+SELECT * FROM [dbo].[product]
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('coca cola', 10,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Pepsi', 12,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Fanta', 15,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Polo Verano', 50,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Hot Dog', 7,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Leche Gloria', 5,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Cocoa', 2,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Nescafe', 3,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Agua Cielo', 25,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Lapiz FaberCastell', 17,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Ayudin', 100,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Tortilla Integral', 4,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Fosforo Llama', 1,1, GETDATE(), 1, GETDATE());
+INSERT INTO [dbo].[product]([description], price, created_by, created_date, updated_by, updated_date) VALUES('Ace', 90,1, GETDATE(), 1, GETDATE());
